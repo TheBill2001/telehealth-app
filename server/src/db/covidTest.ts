3add41d05d6b5d1e9a934072b11f5d138598fe4a
Trần Nam Tuấn <Trn Nam Tun>
2024-01-08 12:38:59 +0700

    Add covid test db

---
+++ server/src/db/covidTest.ts
@@ -0,0 +1,47 @@
+import database from "./connection.js";
+
+export enum CovidTestType {
+    SelfReport = 0,
+    TestingFacility = 1,
+}
+
+export interface ICovidTestResult {
+    user: database.Types.ObjectId;
+    positive: boolean;
+    testingFacility?: string;
+    type: CovidTestType;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const covidTestResultSchema = new database.Schema<ICovidTestResult>(
+    {
+        user: {
+            type: database.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        positive: {
+            required: true,
+            type: Boolean,
+            default: false,
+        },
+        testingFacility: {
+            type: String,
+            required: false,
+        },
+        type: {
+            type: Number,
+            enum: CovidTestType,
+            default: CovidTestType.SelfReport,
+        },
+    },
+    { timestamps: true },
+);
+
+const CovidTest = database.model<ICovidTestResult>(
+    "CovidTest",
+    covidTestResultSchema,
+);
+
+export default CovidTest;
